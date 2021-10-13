a383d83d0da17e62e2ed5496ac69a362a7446332
tszyborski <tszyborski@sii.pl>
2021-10-13 08:39:09 +0200

    elegant python

--- custom_collector_package/restful_booker_collector.py
+++ custom_collector_package/restful_booker_collector.py
@@ -1,18 +1,21 @@
+import requests
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
-import time
-import requests
 
-
-# from custom_collector_package.smoke_test import *
 
 def check_login():
     authentication_url = 'https://restful-booker.herokuapp.com/auth'
-    payload = "{\n    \"username\" : \"admin\",\n    \"password\" : \"password123\"\n}"
+    payload = {
+        "username": "admin",
+        "password": "password123"
+    }
+
     headers = {
         'Content-Type': 'application/json'
     }
-    response = requests.post(authentication_url, headers=headers,
+
+    response = requests.post(authentication_url,
+                             headers=headers,
                              data=payload)
     json_res = response.json()
     access_token = json_res['token']
@@ -29,24 +32,16 @@ def check_bookings():
 class RestfulCustomBookerCollector(object):
     def collect(self):
         access_token = check_login()
-        is_token_retrieved = 0
-        if access_token is None:
-            is_token_retrieved = 0
-        else:
-            is_token_retrieved = 1
-
-        metric_was_login_successful = GaugeMetricFamily('access_token_retrieved', 'Retrieval of access token',
-                                                        value=is_token_retrieved)
+        token_retrieved = 1 if access_token else 0
+        metric_was_login_successful = GaugeMetricFamily('access_token_retrieved',
+                                                        'Retrieval of access token',
+                                                        value=token_retrieved)
         yield metric_was_login_successful
         bookings = check_bookings()
-        are_bookings_retrieved = 0
-        if bookings is None:
-            are_bookings_retrieved = 0
-        else:
-            are_bookings_retrieved = 1
-
-        metric_are_there_any_bookings = GaugeMetricFamily('booking_id_retrieved', 'Retrieval of existing bookings',
-                                                          value=are_bookings_retrieved)
+        bookings_retrieved = 1 if bookings else 0
+        metric_are_there_any_bookings = GaugeMetricFamily('booking_id_retrieved',
+                                                          'Retrieval of existing bookings',
+                                                          value=bookings_retrieved)
         yield metric_are_there_any_bookings
 
 
